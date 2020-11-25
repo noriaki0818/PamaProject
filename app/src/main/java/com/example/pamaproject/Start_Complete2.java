1e9373a47f04db0642b9943da07dc6811674c584
noriaki0818 <n.araya.sys19@morijyobi.ac.jp>
2020-11-25 14:14:02 +0900

    パーマプロジェクトZZ

---
+++ app/src/main/java/com/example/pamaproject/Start_Complete2.java
@@ -0,0 +1,14 @@
+package com.example.pamaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_Complete2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__complete2);
+    }
+}
---
+++ app/src/main/java/com/example/pamaproject/Start_Userinfo2.java
@@ -0,0 +1,14 @@
+package com.example.pamaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_Userinfo2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__userinfo2);
+    }
+}
