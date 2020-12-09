9b6a0b6d4890bcc3301b4e7afc8b4dde24da171a
kositasyouya <s.koshita.sys19@morijyobi.ac.jp>
2020-12-09 11:44:45 +0900

    12/09

--- app/src/main/java/com/example/pamaproject/Home_summary.java
+++ app/src/main/java/com/example/pamaproject/Home_summary.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Home_record extends AppCompatActivity {
+public class Home_summary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_record);
+        setContentView(R.layout.activity_home_summary);
     }
 }
---
+++ app/src/main/java/com/example/pamaproject/article.java
@@ -0,0 +1,14 @@
+package com.example.pamaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class article extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+    }
+}
