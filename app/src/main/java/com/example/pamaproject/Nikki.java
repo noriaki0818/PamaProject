935615039403964b59667f8bd759139583da8185
kositasyouya <s.koshita.sys19@morijyobi.ac.jp>
2021-01-20 15:02:10 +0900

    1/20 匠哉2

--- app/src/main/java/com/example/pamaproject/Nikki.java
+++ app/src/main/java/com/example/pamaproject/Nikki.java
@@ -6,7 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Nikki extends AppCompatActivity implements View.OnClickListener {
     ImageButton bo_tukibetu,bo_record,bo_nikki,bo_article,bo_summary;
@@ -34,12 +41,44 @@ public class Nikki extends AppCompatActivity implements View.OnClickListener {
         Toast.makeText(this,m,Toast.LENGTH_SHORT).show();
 
 
+        //dbから持ってきたデータをlistに入れる
+        //日
+        List<String> hi = new ArrayList<>();
+        hi.add("1");
+        hi.add("2");
+        hi.add("3");
+        hi.add("4");
+        //内容
+        List<String> nai = new ArrayList<>();
+        nai.add("さ");
+        nai.add("む");
+        nai.add("ら");
+        nai.add("い");
 
 
 
+        // listを配列に入れる
+        String[] hiniti = hi.toArray(new String[hi.size()]);
+        String[] naiyou = nai.toArray(new String[hi.size()]);
 
+        // ListViewに表示するリスト項目をArrayListで準備する
+        List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+        for (int i=0; i<hiniti.length; i++){
+            Map<String, String> item = new HashMap<String, String>();
+            item.put("hiniti", hiniti[i]);
+            item.put("naiyou", naiyou[i]);
+            data.add(item);
+        }
 
+        // リスト項目とListViewを対応付けるArrayAdapterを用意する
+        SimpleAdapter adapter = new SimpleAdapter(this, data,
+                android.R.layout.simple_list_item_2,
+                new String[] { "hiniti", "naiyou" },
+                new int[] { android.R.id.text1, android.R.id.text2});
 
+        // ListViewにArrayAdapterを設定する
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
 
     }
